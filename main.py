edecd54daea8e5d908a85e73ffa0894aec6de531
abhaya <gautamabhaya98@gmail.com>
2025-08-16 02:10:19 +0545

    playing around w fastapi

--- main.py
+++ main.py
@@ -6,3 +6,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Welcome to abhaya.dev!"}
+
+
+@app.get("/blogs/")
+async def blog():
+    return {"Blog list will feature here."}
+
+
+@app.get("/blogs/{id}")
+async def get_blog_id(id: int):
+    return {"blog_id": id}
+
+
+@app.get("/gallery/")
+async def get_gallery():
+    return {"Gallery list will feature here."}
+
+
+@app.get("/gallery/photo/{id}")
+async def get_gallery_item_id(id: int):
+    return {"gallery_id": id}
