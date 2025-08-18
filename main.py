d51f5554eaf39513c9c7b72ff45d3bd8159c553b
abhaya <gautamabhaya98@gmail.com>
2025-08-18 23:55:34 +0545

    wip: implemented template for the homepage

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"Welcome to abhaya.dev!"}
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="homepage.html",
+    )
 
 
 @app.get("/blogs/")
@@ -18,11 +27,11 @@ async def get_blog_id(id: int):
     return {"blog_id": id}
 
 
-@app.get("/gallery/")
+@app.get("/photo/")
 async def get_gallery():
     return {"Gallery list will feature here."}
 
 
-@app.get("/gallery/photo/{id}")
+@app.get("/photos/photo/{id}")
 async def get_gallery_item_id(id: int):
     return {"gallery_id": id}
