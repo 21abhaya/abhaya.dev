d06ab57ecb43ea12afe668a8f06c77740f5d2661
abhaya <gautamabhaya98@gmail.com>
2025-09-16 23:23:05 +0545

    restructured project as mvc
    setup database

---
+++ app/db/database.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
+port = os.getenv('DB_PORT')
+
+DATABASE_URL = "mysql+pymysql://user:password@@localhost:port/database"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
