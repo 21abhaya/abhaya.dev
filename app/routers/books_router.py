7a77ec079634d2549e0fd68e6fba3cff3ab77982
abhaya <gautamabhaya98@gmail.com>
2025-09-20 00:53:08 +0545

    fix: static files are now being served correctly from the root file
    learn: grasped a bit on how to manage separation of concerns between the root main.py and other router.py files
    
    todo: learn about dependency injection for implementing templates and other dependencies
    
    todo: start implementing CRUD and also set up htmx

--- app/routers/books_router.py
+++ app/routers/books_router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+
+
+
 
 router = APIRouter()
-
-router.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 
